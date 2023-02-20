d1c08d220eb206e8438a6860131fdc55bc974ad5
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-02-19 22:52:30 -0800

    form creation is working

--- src/index-v2.js
+++ src/index-v2.js
@@ -17,25 +17,50 @@
 
 const game = (() => {
     const formContainer = document.querySelector(".name-form")
-    const newForm = () => {
-        const form = () => {
-            const form = formContainer.appendChild(document.createElement("form"))
-            form.setAttribute("class", "new-player-form");
+    const newForm = (() => {
+        let form = null
+        const createForm = () => {
+            console.log("form created")
+            const playerForm = formContainer.appendChild(document.createElement("form"))
+            playerForm.setAttribute("class", "new-player-form");
+            form = playerForm
         }
         const legend = () => {
-            
+            const legend = form.appendChild(document.createElement("legend"));
+            legend.setAttribute("class", "")
+            return legend
         }
-        const label = () => {}
-        const input = () => {}
-        const submit = () => {}
+        const label = () => {
+            const label = form.appendChild(document.createElement("label"))
+            label.setAttribute("for", "player-name")
+            label.setAttribute("class","")
+            label.textContent = "Enter your Name!"
+            return label
+        }
+        const input = () => {
+            const input = form.appendChild(document.createElement("input"));
+            input.setAttribute("class", "")
+            input.setAttribute("id","player-name")
+            input.setAttribute("type", "text")
+            return input
+        }
+        const submit = () => {
+            const submit = form.appendChild(document.createElement("button"));
+            submit.setAttribute("id", "");
+            submit.setAttribute("class", "");
+            submit.setAttribute("type", "submit");
+            submit.textContent = "Start Game"
+            return submit
+        }
 
-    };
-
-    
-    return {createForm}
+        return { form, createForm, legend, label, input, submit};
+    })();
+    return {newForm}
 })();
 
-game.createForm()
+
+
+
 
 // gameboard module
 const gameBoard = (() => {
@@ -55,8 +80,20 @@ const gameBoard = (() => {
     const events = () => {
         const startButton = document.querySelector("#start-button")
         startButton.addEventListener("click", ()=>{
+
+            function playerForm(){
+                game.newForm.createForm()
+                game.newForm.legend()
+                game.newForm.label()
+                game.newForm.input()
+                game.newForm.submit()
+            };
+            
+            function setUp(){}
         })
     };
     return {board, renderBoard, events}
 })();
+
+gameBoard.events()
 
