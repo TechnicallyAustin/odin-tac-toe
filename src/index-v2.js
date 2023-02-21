200f426dccf75f86274f19351e42bbe94c5a0c27
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-02-20 20:10:35 -0800

    working on creating players and allowing them to add on div click

--- src/index-v2.js
+++ src/index-v2.js
@@ -1,20 +1,3 @@
-// Single item = module
-// multiple Items = factory
-
-//save array inside gameBoard{}
-// renderBoard()
-
-// create name form
-
-// players can add a marker to the board -- updates Dom -- move Validation
-
-// 3 objects gameBoard{} player{} game{}
-
-// checks when game is over
-    // checks win combos
-    // declareeres winer
-    // updates display
-
 const game = (() => {
     const formContainer = document.querySelector(".name-form")
     const newForm = (() => {
@@ -23,10 +6,24 @@ const game = (() => {
             console.log("form created")
             const playerForm = formContainer.appendChild(document.createElement("form"))
             playerForm.setAttribute("class", "new-player-form");
-            playerForm.addEventListener("submit", (event) => {
-                event.preventDefault()
-            })
             form = playerForm
+
+            form.addEventListener("submit", (event) =>{
+                event.preventDefault();
+                let name = document.querySelector("#player-name").value
+                console.log("from form", name)
+
+                const newPlayer = Player(name, "X")
+                console.log("form", newPlayer.getName(), newPlayer.getMarker())
+
+                   console.log("form submitted");
+
+                   gameBoard.renderBoard();
+                   gameBoard.events();
+
+
+
+            })
         }
         const legend = () => {
             const legend = form.appendChild(document.createElement("legend"));
@@ -54,30 +51,61 @@ const game = (() => {
             submit.setAttribute("class", "");
             submit.setAttribute("type", "submit");
             submit.textContent = "Start Game"
-
-            submit.addEventListener("click", ()=>{
-                console.log("form submitted")
-                gameBoard.renderBoard()
-                gameBoard.events()
-
-            })
             return submit
         }
 
         return { form, createForm, legend, label, input, submit};
     })();
 
+    const gameStart = () => {
+      const startButton = document.querySelector("#start-button");
+
+      
+      startButton.addEventListener("click", () => {
+          const form = document.querySelector(".new-player-form")
+          if (form === null){
+              game.newForm.createForm();
+              game.newForm.legend();
+              game.newForm.label();
+              game.newForm.input();
+              game.newForm.submit();
+          } else {console.log("form exists")};
+            
+      });
+    };
+
     const display = (() => {
-
+        // sets the display to the name of the current player
     })
-    return {newForm, display}
+
+    const winCombos = (() => {
+        const wins = (array) => {
+            [ // horizontal
+              [array[0], array[1], array[2]],
+              [array[3], array[4], array[5]],
+              [array[6], array[7], array[8]],
+              // vertical
+              [array[0], array[3], array[6]],
+              [array[1], array[4], array[7]],
+              [array[2], array[5], array[8]],
+              // diagonal
+              [array[0], array[4], array[8]],
+              [array[2], array[4], array[6]],
+            ];
+        };
+        return { wins}
+    })(); // takes an array as an argument to check win combos
+
+    const winner = () => {
+    } //declares a winner based on win combos and markers.
+
+    const round = () => {} // returns the current round.
+
+    return {newForm, display, winCombos, gameStart, winner, round}
 })();
+game.gameStart()
 
-const player = (name, marker) => {}
-
-
-
-
+// Playere factory Funcion
 
 // gameboard module
 const gameBoard = (() => {
@@ -85,48 +113,45 @@ const gameBoard = (() => {
         "","","",
         "","","",
         "","",""]; // defines the gameboard as an empty array
-
-    const renderBoard = () => {
-        const container = document.querySelector(".board")
-        console.log(container)
-        for (let i = 0; i < board.length; i ++){
-            let div = container.appendChild(document.createElement("div"));
-            div.setAttribute("class", `${i} bg-warning col border`);
-            div.textContent = board[i]
-        }
-    }; // renders the game baord
-
-    const gameForm = () => {
-        game.newForm.createForm()
-        game.newForm.legend()
-        game.newForm.label()
-        game.newForm.input()
-        game.newForm.submit()
-    }; // organizes form creation methods
-
-    
-    const gameStart = () => {
-            const startButton = document.querySelector("#start-button");
-            startButton.addEventListener("click", ()=>{
-                gameForm()
-            })
-        }; // calls gameForm 
-        gameStart()
-
-    const events = () => {
-        const board = document.querySelector(".board");
-
-        for (let i = 0; i < board.children.length; i++){
-            let div = board.children[i]
-            div.addEventListener("click", () => {
-                console.log("add player marker here")
-                console.log("board location", `${i}`)
+        
+        const renderBoard = () => {
+            const container = document.querySelector(".board")
+            console.log(container)
+            for (let i = 0; i < board.length; i ++){
+                let div = container.appendChild(document.createElement("div"));
+                div.setAttribute("class", `${i} bg-warning col border`);
+                div.textContent = board[i]
+            }
+        }; // renders the game baord
+            
+        const events = () => {
+            const board = document.querySelector(".board")
+            for (let i = 0; i < board.children.length; i++){
+                let div = board.children[i]
+                div.addEventListener("click", () => {
+                    console.log("add player marker here")
+                    console.log("board location", `${i}`)
             })
         }
     }; // contains logic for gameboard div events
     
-    
-    return {board, renderBoard, gameForm, events}
+    return {board, renderBoard, events}
 })();
 
 
+// Playere factory Funcion
+const Player = (name, marker) => {
+    const getMarker = () => {
+        console.log(marker)
+        return marker
+    }
+    const add = () => {
+        console.log("added");
+    }; // adds the player marker to the selected board div
+    const getName = () => {
+        console.log(name)
+        return name
+    }; // given form input sets the name of the player.
+
+  return { add, getName, getMarker};
+};
