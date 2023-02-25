d08df30283799f0cc3a648d6cf82e0aeee88108a
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-02-25 01:41:37 -0800

    working on win evaluation pending refactor

--- src/index-v2.js
+++ src/index-v2.js
@@ -125,7 +125,7 @@ const playRound = (marker, board, index, event) => {
     console.log(event)
     if (board[index] === "") {
         game.currentPlayer.add(marker, board, index, event)
-
+        console.log(gameBoard.winCombos(gameBoard.board))
     }
 }
 
@@ -138,7 +138,7 @@ const playRound = (marker, board, index, event) => {
         game.currentPlayer = game.playerTwo
     }
   game.display()
-  game.winner() // checks for a winner
+  console.log(gameBoard.winCombos(gameBoard.board)) // checks for a winner
   game.currentRound++
   }; // if the current round is divisible cleanly by 2 its playerOne's turn
 
@@ -147,31 +147,14 @@ const playRound = (marker, board, index, event) => {
     display.textContent = `${game.currentPlayer.getName()}. You are ${game.currentPlayer.getMarker()}`;
   };
 
-  const winCombos = (() => {
-    const wins = (array) => {
-      [
-        // horizontal
-        [array[0], array[1], array[2]],
-        [array[3], array[4], array[5]],
-        [array[6], array[7], array[8]],
-        // vertical
-        [array[0], array[3], array[6]],
-        [array[1], array[4], array[7]],
-        [array[2], array[5], array[8]],
-        // diagonal
-        [array[0], array[4], array[8]],
-        [array[2], array[4], array[6]],
-      ];
-    };
-    return { wins };
-  })(); // takes an array as an argument to check win combos
+
+// takes an array as an argument to check win combos
 
   const winner = () => {}; //declares a winner based on win combos and markers.
 
   return {
     newForm,
     display,
-    winCombos,
     playerOne,
     playerTwo,
     playRound,
@@ -192,34 +175,64 @@ const gameBoard = (() => {
         "","","",
         "","",""]; // defines the gameboard as an empty array
         
-        const renderBoard = () => {
-            const container = document.querySelector(".board")
-            console.log(container)
-            for (let i = 0; i < board.length; i ++){
-                let div = container.appendChild(document.createElement("div"));
-                div.setAttribute("class", `board-item ${i} bg-warning col border fs-1 d-flex justify-content-center align-items-center`);
-                div.textContent = board[i]
-            }
-        }; // renders the game baord
+    const renderBoard = () => {
+        const container = document.querySelector(".board")
+        console.log(container)
+        for (let i = 0; i < board.length; i ++){
+            let div = container.appendChild(document.createElement("div"));
+            div.setAttribute("class", `board-item ${i} bg-warning col border fs-1 d-flex justify-content-center align-items-center`);
+            div.textContent = board[i]
+        }
+    }; // renders the game baord
             
-        const events = () => {
-            const board = document.querySelector(".board")
-            for (let i = 0; i < board.children.length; i++){
-                let div = board.children[i]
-                div.addEventListener("click", (event) => {
-                    const cell = event.target
-                    game.playRound(
-                      game.currentPlayer.getMarker(),
-                      gameBoard.board,
-                      i,
-                      cell
-                    ); // adds player marker to the board array
-                    //game.display()// adds player Marker to the Dom
+    const events = () => {
+        const board = document.querySelector(".board")
+        for (let i = 0; i < board.children.length; i++){
+            let div = board.children[i]
+            div.addEventListener("click", (event) => {
+                const cell = event.target
+                game.playRound(
+                  game.currentPlayer.getMarker(),
+                  gameBoard.board,
+                  i,
+                  cell
+                ); // adds player marker to the board array
+                //game.display()// adds player Marker to the Dom
             });
         }
     }; // contains logic for gameboard div events
+
+     const winCombos = array => {
+
+        // horizontal
+          const combos = [
+                [array[0], array[1], array[2]],
+                [array[3], array[4], array[5]],
+                [array[6], array[7], array[8]],
+                // vertical
+                [array[0], array[3], array[6]],
+                [array[1], array[4], array[7]],
+                [array[2], array[5], array[8]],
+                // diagonal
+                [array[0], array[4], array[8]],
+                [array[2], array[4], array[6]]
+            ]
+
+            for (let i = 0; i < combos.length; i++){
+                let win = combos[i]
+
+          //     let winning = win.filter(element => element.every(item => item === "X"|"O")}
+
+               if (winning) {
+                console.log("Winner")
+               }
+
+
+
+            }
+        }
     
-    return {board, renderBoard, events}
+    return {board, renderBoard, events, winCombos}
 })();
 
 
